7d556b9da99a6c72c70f032217d82015083d46e4
Larry <larry.fenn@gmail.com>
2015-11-10 22:56:31 -0500

    * data.csv: file that will contain match IDs and pick/ban lists * httprequest.py: runs indefinitely, looking at 100 records at a time   from the Dota 2 web API * record.dat: last match ID record for program execution

--- httprequest.py
+++ httprequest.py
@@ -3,18 +3,33 @@ import urllib.request
 # python 2.7
 #import urllib2
 import json
-#get last 100
-lastid = open('record.dat', 'r').read()
+import time
+recordfile = open('record.dat', 'r')
+lastid = recordfile.read()
+recordfile.close()
+
 apikey = open('key.dat', 'r').read()
-data = urllib.request.urlopen("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?min_players=10&key=" + apikey + "&skill=3&start_at_match_id=" + lastid).read()
-#todo: add in file i/o, track last match processed, to automatically sift through all of them
-#ultimately want to set up a CRON job and store the things in a database (just a CSV?)
-#implement a try catch in case of error 503
-matches = json.loads(data)
-for i in range(100):
-	match_id = matches['result']['matches'][i]['match_id']
-	match_data = json.loads(urllib2.urlopen("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id= " + str(match_id) + "&key=" + apikey).read())
-	print(match_data['result']['game_mode'])
-	if (match_data['result'].has_key('picks_bans')):
-		print(match_data['result']['picks_bans'])
-		#print(match_data['result']['duration'])
+
+while (True):
+	matches = open('data.csv', 'a')
+	data = json.loads(urllib.request.urlopen("https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?min_players=10&key=" + apikey + "&skill=3&start_at_match_id=" + lastid).read().decode("utf-8"))
+	#implement a try catch in case of error 503
+	for i in range(100):
+		match_id = data['result']['matches'][i]['match_id']
+		print(match_id)
+		match_data = json.loads(urllib.request.urlopen("https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?key=" + apikey + "&match_id=" + str(match_id)).read().decode("utf-8"))
+		if 'picks_bans' in match_data['result']:
+			matches.write(str(match_id) + ",")
+			matches.write(str(match_data['result']['radiant_win']) + ",")
+			matches.write(str(match_data['result']['duration']))
+			picklist = match_data['result']['picks_bans']
+			for i in range(len(picklist)):
+				matches.write("," + str(picklist[i]['order']) + "," + str(picklist[i]['is_pick']) + "," + str(picklist[i]['hero_id']) + "," + str(picklist[i]['team']))
+			matches.write("\n")
+	matches.close()
+	lastid = match_id
+	recordfile = open('record.dat', 'w')
+	recordfile.write(str(lastid))
+	recordfile.close()
+	print("CYCLE FINISHED")
+	time.sleep(5)
